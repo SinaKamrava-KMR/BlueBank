4b42c142769a6f4051bdc7910571a901e8dfe9dc
Sina Kamrava <sinakamrava3118@gmail.com>
2023-04-23 23:44:35 +0330

    create blue bank landing page

---
+++ public/app.js
@@ -0,0 +1,32 @@
+const slider = document.getElementById('container')
+const cards = document.getElementById('cards');
+let isPressed = false;
+let cursorX;
+
+slider.addEventListener("mousedown", (e) => {
+  isPressed = true;
+  cursorX = e.offsetX - cards.offsetLeft;
+  // slider.style.cursor = "grabbing";
+});
+
+window.addEventListener("mouseup", () => {
+  isPressed = false;
+});
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isPressed) return;
+  e.preventDefault();
+  cards.style.left = `${e.offsetX - cursorX}px`;
+  boundSlides();
+});
+
+function boundSlides() {
+  const containerRect = slider.getBoundingClientRect();
+  const cardsRect = cards.getBoundingClientRect();
+
+  if (parseInt(cards.style.left) > 0) {
+    cards.style.left = 0;
+  } else if (cardsRect.right < containerRect.right) {
+    cards.style.left = `-${cardsRect.width - containerRect.width}px`;
+  }
+} 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,19 @@ module.exports = {
         "IranyekanBold":["IranyekanBold","sans-serif"],
         "IranyekanThin":["IranyekanThin","sans-serif"],
         "IranyekanRegular":["yekanRegular","sans-serif"],
+      },
+      keyframes: {
+        move: {
+          '0%': { transform: 'translateY(0)' },
+          '50%':{ transform: 'translateY(20px)' },
+          '100%': { transform: 'translateY(0)' },
+        }
+      },
+      animation: {
+        move: 'move 5s ease-in-out  infinite ',
+      },
+      boxShadow: {
+        'inset': '0 0 15px',
       }
     },
   },
